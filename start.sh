952be93e2b248aa182af7e13fa321bc78337286e
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-02-15 22:48:53 -0500

    Deploying to heroku

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export SECRET_KEY= Moringa school
 export UPLOADED_PHOTOS_DEST =app/static/photos
+export SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://moringa:access@localhost/blog
 python3 manage.py server
