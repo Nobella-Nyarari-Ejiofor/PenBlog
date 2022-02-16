2294e1302148a86437862190c3ecaf4d5a5cf6bb
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-02-15 23:30:11 -0500

    Deploying to heroku

--- config.py
+++ config.py
@@ -2,16 +2,18 @@ import os
 
 class Config:
   SECRET_KEY = 'Moringa School'
-  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:access@localhost/blog'
   UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
+
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+
 
 
 class DevConfig(Config):
-    DEBUG = True
+    # DEBUG = True
+    pass
 
 config_options = {
 'development':DevConfig,
